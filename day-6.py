e8e70b6e2bbaad6bc2cf3d0af56d97cb0dd5a2c5
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-02-22 21:32:13 +0100

    Add outline for day six challenge

---
+++ day-6.py
@@ -0,0 +1,16 @@
+# Advent of Code Christmas Calendar, Day #6
+
+# Import required libraries
+from config import fetch_data
+
+# Set URL for the day
+url = 'https://adventofcode.com/2023/day/6/input'
+
+# Define main function
+def main():
+    # Fetch input data from Advent of Code website
+    data = fetch_data(url)
+
+# Call the main function
+if __name__ == "__main__":
+    main()
