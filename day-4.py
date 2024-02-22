8c68cd4bf5e37adaf10df4cb7533b2db77f4a840
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-02-22 21:16:55 +0100

    Create outline for day four challenge

---
+++ day-4.py
@@ -0,0 +1,15 @@
+# Advent of Code Christmas Calendar, Day #4
+
+# Import required libraries
+from config import fetch_data
+
+# Set URL for the day
+url = 'https://adventofcode.com/2023/day/4/input'
+
+def main():
+    # Fetch input data from Advent of Code website
+    data = fetch_data(url)
+
+# Call the main function
+if __name__ == "__main__":
+    main()
